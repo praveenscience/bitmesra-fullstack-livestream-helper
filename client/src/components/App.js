5e40b461aba635ee087431de502f4eed84f225ae
Praveen Kumar <praveenscience@gmail.com>
2022-02-12 13:14:09 +0000

    Updated the MockData and fixed the rendering of Data.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Bootstrap/Header";
+import Card from "./Bootstrap/Card";
+import Data from "../constants/MockData.json";
 
 class App extends Component {
   render() {
@@ -8,6 +10,35 @@ class App extends Component {
         <Header dark={true} className="Header justify-content-center">
           LiveStream Helper
         </Header>
+        <div className="container-fluid">
+          <div className="row">
+            {Data.map((str, key) => (
+              <div className="col-12 col-sm-6 col-lg-4 col-xl-3 mb-4" key={key}>
+                <Card
+                  Image={
+                    str.Designs.Thumbnail
+                      ? `https://i.imgur.com/${str.Designs.Thumbnail}l.png`
+                      : "https://i.imgur.com/TREuBIcl.png"
+                  }
+                  ImgAlign="top"
+                  Header={str.Title}
+                >
+                  <Card
+                    Header="YouTube"
+                    TextHeader={true}
+                    Title={str.YouTube.Title}
+                  >
+                    <p className="card-text YouTube-Desc">
+                      {str.YouTube.Description
+                        ? JSON.parse(`"${str.YouTube.Description}"`)
+                        : ""}
+                    </p>
+                  </Card>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
