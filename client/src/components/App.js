b7d86efb4f3fd6e428dcb2c8befe26806efc117b
Praveen Kumar <praveenscience@gmail.com>
2022-02-12 12:38:13 +0000

    Added a header component.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Header from "./Bootstrap/Header";
 
 class App extends Component {
   render() {
-    return <div className="App">LiveStream Helper</div>;
+    return (
+      <div className="App">
+        <Header dark={true} className="Header mb-3 justify-content-center">
+          LiveStream Helper
+        </Header>
+      </div>
+    );
   }
 }
 
